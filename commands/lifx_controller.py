ae6ec9b8acb0b5315ab90734defd2841fa556784
eggyolkmedia <github@eggyolkmedia.com>
2016-07-28 20:08:18 +0300

    Adding LIFX controller (very basic)

---
+++ commands/lifx_controller.py
@@ -0,0 +1,29 @@
+import requests
+
+class LifxControllerCommand (command.AbstractCommand) :
+
+  def run(self, args, logger=None) :
+    group = args['group']
+    state = args['state']
+    fade = args['fade']
+
+    token = self.params['auth_token']
+
+    self._debug('setting {} {}'.format(group, state))
+  
+    headers = {
+      "Authorization": "Bearer %s" % token,  
+    }
+
+    payload = {
+      "power": state,
+      "duration": fade
+    }
+
+    response = requests.put('https://api.lifx.com/v1/lights/group:{}/state'.format(group), data=payload, headers=headers)
+    self._debug(response.content)
+    
+
+  def _debug(self, message, logger) :
+    if logger :
+      logger.log('[DEBUG] ' + message)
